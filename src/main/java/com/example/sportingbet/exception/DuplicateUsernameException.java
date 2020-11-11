77f85f639da40ff342621c4b16c339e96d72aeeb
CatalinSimion032398 <simion.catalin2398@gmail.com>
2020-11-11 12:56:02 +0200

    -create class ErrorRespoonse in pachage exception -create a custom WhiteLabel

--- src/main/java/com/example/sportingbet/exception/DuplicateUsernameException.java
+++ src/main/java/com/example/sportingbet/exception/DuplicateUsernameException.java
@@ -1,5 +1,6 @@
 package com.example.sportingbet.exception;
 
+
 public class DuplicateUsernameException extends Exception {
     public DuplicateUsernameException(String string) {
         super(string);
